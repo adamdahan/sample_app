4bf0d36a3bed7ab53b121dc8bdd120ba873a6d2e
adamdahan <adam@88labs.com>
2017-01-07 17:01:45 -0500

    updates to try to fix forgot password

--- config/routes.rb
+++ config/routes.rb
@@ -3,28 +3,18 @@ Rails.application.routes.draw do
 
   get 'password_resets/edit'
 
+  get 'account_activations/edit'
+
   get 'sessions/new'
-  get 'users/new'
 
-  # root view
-  root 'static_pages#home'
-
-  # static pages
-  get  '/help',    to: 'static_pages#help'
-  get  '/about',   to: 'static_pages#about'
-  get  '/contact', to: 'static_pages#contact'
-
-  #signup
-  get  '/signup',  to: 'users#new'
-  post '/signup',  to: 'users#create'
-
-  # login
-  get    '/login',   to: 'sessions#new'
-  post   '/login',   to: 'sessions#create'
-
-  # logout
-  delete '/logout',  to: 'sessions#destroy'
-
+  root                'static_pages#home'
+  get    'help'    => 'static_pages#help'
+  get    'about'   => 'static_pages#about'
+  get    'contact' => 'static_pages#contact'
+  get    'signup'  => 'users#new'
+  get    'login'   => 'sessions#new'
+  post   'login'   => 'sessions#create'
+  delete 'logout'  => 'sessions#destroy'
   resources :users
   resources :account_activations, only: [:edit]
   resources :password_resets,     only: [:new, :create, :edit, :update]
