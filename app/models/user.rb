56017a21108d222f1e2f4a8dd9dfeacac499f59a
adamdahan <adam@88labs.com>
2017-01-07 17:26:22 -0500

    updates to michael hartls code

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,18 +1,14 @@
-class User < ApplicationRecord
-
+class User < ActiveRecord::Base
   attr_accessor :remember_token, :activation_token, :reset_token
-
   before_save   :downcase_email
   before_create :create_activation_digest
-
   validates :name,  presence: true, length: { maximum: 50 }
-  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\-.]+\.[a-z]+\z/i
   validates :email, presence: true, length: { maximum: 255 },
                     format: { with: VALID_EMAIL_REGEX },
                     uniqueness: { case_sensitive: false }
   has_secure_password
   validates :password, presence: true, length: { minimum: 6 }, allow_nil: true
-
 
   # Returns the hash digest of the given string.
   def User.digest(string)
@@ -32,16 +28,16 @@ class User < ApplicationRecord
     update_attribute(:remember_digest, User.digest(remember_token))
   end
 
+  # Forgets a user.
+  def forget
+    update_attribute(:remember_digest, nil)
+  end
+
   # Returns true if the given token matches the digest.
   def authenticated?(attribute, token)
     digest = send("#{attribute}_digest")
     return false if digest.nil?
     BCrypt::Password.new(digest).is_password?(token)
-  end
-
-  # Forgets a user.
-  def forget
-    update_attribute(:remember_digest, nil)
   end
 
   # Activates an account.
@@ -55,21 +51,21 @@ class User < ApplicationRecord
     UserMailer.account_activation(self).deliver_now
   end
 
-  # Returns true if a password reset has expired.
-  def password_reset_expired?
-    reset_sent_at < 2.hours.ago
-  end
-
   # Sets the password reset attributes.
   def create_reset_digest
     self.reset_token = User.new_token
-    update_attribute(:reset_digest,  User.digest(reset_token))
+    update_attribute(:reset_digest, User.digest(reset_token))
     update_attribute(:reset_sent_at, Time.zone.now)
   end
 
   # Sends password reset email.
   def send_password_reset_email
     UserMailer.password_reset(self).deliver_now
+  end
+
+  # Returns true if a password reset has expired.
+  def password_reset_expired?
+    reset_sent_at < 2.hours.ago
   end
 
   private
