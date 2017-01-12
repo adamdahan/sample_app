2199d1c26e9bd4959a5231dc3001ec994f3311ae
michaelReyder <michaelreyder@gmail.com>
2017-01-11 17:27:41 -0700

    Updated omniauth permissions. Added call to graph api.

--- app/models/user.rb
+++ app/models/user.rb
@@ -86,6 +86,10 @@ class User < ActiveRecord::Base
   def password_reset_expired?
     reset_sent_at < 2.hours.ago
   end
+  
+  def facebook
+	@facebook ||= Koala::Facebook::API.new(oauth_token)
+  end
 
   private
 
--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,7 +1,3 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :facebook, FACEBOOK_CONFIG['app_id'], FACEBOOK_CONFIG['secret'], {:scope => 'public_profile,email'}
-end
-
-OmniAuth.config.on_failure = Proc.new do |env|
-  SessionsController.action(:auth_failure).call(env)
+  provider :facebook, FACEBOOK_CONFIG['app_id'], FACEBOOK_CONFIG['secret'], {:scope => 'public_profile,email,user_events'}
 end
